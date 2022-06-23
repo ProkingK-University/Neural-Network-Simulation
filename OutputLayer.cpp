5da3522d352f6e0248229be534f4464b9b65c99b
Ashley Kapaso <ashleykapaso@gmail.com>
2022-06-23 19:21:34 +0200

    Implementation of OutputLayer

--- OutputLayer.cpp
+++ OutputLayer.cpp
@@ -1,36 +1,52 @@
 #include "OutputLayer.h"
 
-OutputLayer::OutputLayer(int numWeights, double* weights)
+OutputLayer::OutputLayer(int numWeights, double* weights) : numWeights(numWeights)
 {
+    outputValue = 0;
 
+    this->weights = weights;
 }
 
 void OutputLayer::printLayer()
 {
-
+    std::cout<< "o:1" <<std::endl;
 }
 
 void OutputLayer::clearLayer()
 {
-
+    outputValue = 0;
 }
 
 void OutputLayer::setsOutputValue(double outputValue)
 {
-
+    this->outputValue = outputValue;
 }
 
 void OutputLayer::forward(HiddenLayer* lastHiddenLayer)
 {
+    double total = 0;
+    HiddenNeuron** prevNeurons = lastHiddenLayer->getNeurons();
 
+    for (int i = 0; i < numWeights; i++)
+    {
+        total += prevNeurons[i]->getValue() * weights[i];
+    }
+     
+    outputValue = total;
 }
 
 double OutputLayer::getOutputValue()
 {
-
+    return outputValue;
 }
 
 OutputLayer::~OutputLayer()
 {
-
+    for (int i = 0; i < numWeights; i++) 
+    {
+        weights[i] = NULL;
+    }
+    delete [] weights;
 }
+
+// (;
